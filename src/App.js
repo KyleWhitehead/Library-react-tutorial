463a62088cad082be4c028e137ebf0dc9c6b916f
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-02-16 10:24:33 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Highlights from "./Components/Highlights";
+import Landing from "./Components/Landing";
+import Nav from "./Components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+    </div>
+  );
+}
+
+export default App;
