771592e871176a337a7254767de17f21b1764159
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-02-24 12:21:25 -0700

    Update .5.6

--- src/Components/Discounted.jsx
+++ src/Components/Discounted.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { books } from "../data";
-import Book from "./UI/Book";
+import Book from "../UI/Book";
 
 const Discounted = () => {
   return (
--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Book from "./UI/Book";
+import Book from "../UI/Book";
 import { books } from "../data.js";
 
 const Featured = () => {
