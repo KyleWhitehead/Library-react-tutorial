1e49f01e7eb3fc0d8759f08b47ad907ddc06cf34
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-02-24 10:30:40 -0700

    Update.5.3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
-import {BrowserRouter as Router, Route } from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
@@ -14,7 +14,7 @@ function App() {
       <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books/1" render={() => <BookInfo books={books} />} />
+      <Route path="/books/:id" render={() => <BookInfo books={books} />} />
       <Footer />
     </div>
     </Router>
